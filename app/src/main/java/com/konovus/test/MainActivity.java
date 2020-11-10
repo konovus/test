3b42ab4e6433507603087f0c0eb4de3ad5c41756
konovus <ciobancostea@gmail.com>
2020-11-10 10:33:40 +0000

    sout3

--- app/src/main/java/com/konovus/test/MainActivity.java
+++ app/src/main/java/com/konovus/test/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("hi GitHub, and back...");
-        System.out.println("...");
+        System.out.println("...starting with master");
     }
 }
